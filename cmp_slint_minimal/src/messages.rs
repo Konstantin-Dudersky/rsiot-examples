91600f15460b9ab54d30f6a5c789ef1ba1cda410
Konstantin-Dudersky <Konstantin.Dudersky@gmail.com>
2026-05-25 21:26:31 +0300

    move cmp_slint_minimal

--- cmp_slint_minimal/src/messages.rs
+++
@@ -1,9 +0,0 @@
-use rsiot::message::{MsgDataBound, MsgKey};
-use serde::{Deserialize, Serialize};
-
-#[derive(Clone, Debug, Deserialize, MsgKey, PartialEq, Serialize)]
-pub enum Msg {
-    ButtonPressed,
-}
-
-impl MsgDataBound for Msg {}
